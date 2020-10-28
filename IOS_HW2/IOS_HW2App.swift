6024099248bd3ab30040ac63eb8f809473f2864c
Eric's Mac <eric17618@gmail.com>
2020-10-29 01:36:07 +0800

    Initial Commit

---
+++ IOS_HW2/IOS_HW2App.swift
@@ -0,0 +1,17 @@
+//
+//  IOS_HW2App.swift
+//  IOS_HW2
+//
+//  Created by 翁星宇 on 2020/10/29.
+//
+
+import SwiftUI
+
+@main
+struct IOS_HW2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
